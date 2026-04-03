28dad12c2b67d7e980a64cd1b05b74d2a66a3cca
thehoang-x-five <thedeptrai10373606590@gmail.com>
2026-04-03 08:25:54 +0700

    week4-dev2

--- obj/Debug/net8.0/HealthCare.AssemblyInfo.cs
+++ obj/Debug/net8.0/HealthCare.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76587a837ac775626094376a5d099082e39f69d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc9598f6e9d6c2b5c0b17989c8ba9c3f6b1260a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
